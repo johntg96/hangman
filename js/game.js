1b5f73ad0b077db16f794f31f1ed69c263448b94
johntg96 <johntgarrison@gmail.com>
2016-09-30 20:29:15 -0700

    Guessing and checking implemented into JS. Preliminary reset functionality added.. Need to fix a few bugs with checking letter guess aftrer a reset -> see console.log output after reset

--- js/game.js
+++ js/game.js
@@ -1,15 +1,26 @@
 // game.js
 
+var numWrong = 0;
+
 // start a new game
 $('#new-game').on('click', function() {
     $('.game').removeClass('hide');
-    $(this).text("Reset");
+    $(this).addClass("hide");
+    $('#reset').removeClass('hide');
     randomWord();
 });
 
-// disable letters when clicked
-$('.letter').on('click', function() {
-    $(this).addClass('letter-disable');
+// reset game
+function reset() {
+    $('.drawing').html("");
+    $('#num-wrong').text("");
+    $('.letter').removeClass('letter-disable');
+    numWrong = 0;
+    randomWord();
+}
+
+$('#reset').on('click', function() {
+    reset();
 });
 
 // request random word from CDN
@@ -20,13 +31,44 @@ function randomWord() {
             type: "GET",
             url: requestStr,
             dataType: "jsonp",
-            jsonpCallback: 'randomWordComplete'
+            jsonpCallback: 'guessWord'
         });
     }
 
-function randomWordComplete(data) {
-    var word = data.Word;
-    alert(word); // testing
-    var spaces = word.length;
-    alert(spaces); // testing
+function guessWord(data) {
+    var word = data.Word.toUpperCase();
+    console.log(word);
+    var wordLetters = word.split("");
+
+    for (var i = 0; i < wordLetters.length; i++) {
+        var letterSpace = "<span class='letter-spaces letter-" + wordLetters[i] + "'>_</span>";
+        $('.drawing').append(letterSpace);
+    }
+
+    $('#num-wrong').text("# wrong guesses: " + numWrong);
+
+    // disable letters when clicked
+    $('.letter').on('click', function() {
+        $(this).addClass('letter-disable');
+        // if letter exists in word
+        console.log(numWrong);
+        var guessLetter = word.indexOf($(this).text());
+        var letterPick = $(this).text();
+        var letterPickID = ".letter-" + letterPick;
+        if (($('letter-spaces').has(letterPickID)) && (guessLetter !== -1) && (numWrong <= word.length + 4)) {
+            $(letterPickID).text(letterPick);
+        } else if (numWrong >= word.length + 4) {
+            $('.drawing').append("<h2 id='game-over'>Game Over!</h2>");
+            endGame();
+        }
+        else {
+            console.log("Wrong");
+            numWrong++;
+            $('#num-wrong').text("# wrong guesses: " + numWrong);
+        }
+    });
+}
+
+function endGame() {
+    console.log("YOU LOST LOSER.");
 }
